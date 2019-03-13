c2f6b35e7be7c0213119c9e0f79a29646d94ea65
Ramon Andrade <ramonandrade@Ramons-MBP.home>
2019-03-13 18:55:58 +0100

    Finishing week 10

---
+++ Week-10/LDSLibrary/LDSLibrary/Controllers/MoviesController2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace LDSLibrary.Controllers
+{
+    public class MoviesController2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Watch(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            ViewData["Title"] = "Watch";
+
+            return View();
+        }
+
+        public string Edit()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public string Delete()
+        {
+            return "This is the Welcome action method...";
+        }
+
+
+    }
+}
---
+++ Week-10/LDSLibrary/LDSLibrary/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LDSLibrary.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required, Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Display(Name = "Video URL (Youtube)")]
+        public string Custom { get; set; }
+    }
+}
